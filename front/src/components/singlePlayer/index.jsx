aada22524c72a69f19d603e94b7c00df0ae5434c
gab1005 <gabrielqwerty1@gmail.com>
2022-09-10 17:03:48 -0300

    workin in name of images of cards, and in the logic to load images acording array parse

--- front/src/components/singlePlayer/index.jsx
+++ front/src/components/singlePlayer/index.jsx
@@ -2,19 +2,44 @@ import ContainerCards from "./elements/ContainerCard";
 import ImagePlayer from "./elements/ImagePlayer";
 import Chips from "./elements/chips";
 import SumCards from "./elements/sumCards";
+// import img from "../../assets/cards/"
+
+
 const exArr = [
-  ["A","C"],
-  ["A","E"],
+  ["A", "C"],
+  ["A", "C"],
+  ["4", "C"],
 ]
-const SinglePlayer = () => {
-  return(
+
+const SinglePlayer = ({ arrayCards }) => {
+  return (
     <section className="w-[30%]  border-4 border-black flex flex-col justify-center items-center" >
       <ContainerCards arrCards={exArr} />
       <ImagePlayer />
-      <Chips value="12.000"/>
-      <SumCards sumOfCards={21} />
+      <Chips value="12.000" />
+      <SumCards sumOfCards={17} />
+      {imageCards(exArr)}
     </section>
   );
 };
 
 export default SinglePlayer;
+
+// function para carregar images
+const imageCards = (arr) => {
+  let imageName = '';
+  const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+
+  return (
+    arr.map((elem) => {
+      const numberCard = elem[0];
+      const suitCard = elem[1];
+      imageName = numberCard + '-' + suitCard + '.png';
+      // console.log(`../../assets/cards/${suitCard}/${imageName} `)
+      <img src={`../../assets/cards/${suitCard}/${imageName} `} ></img>
+    })
+  );
+
+};
+
+// function para somar as cartas
