2e2fe8744a2a6c45de79e45d4a37932206592c42
gab1005 <gabrielqwerty1@gmail.com>
2022-09-17 20:39:19 -0300

    conectado front back para fazer login/cadastro

--- front/src/components/LoginCadastro/index.jsx
+++ front/src/components/LoginCadastro/index.jsx
@@ -1,20 +1,36 @@
 import { useState } from 'react';
+import { ethers } from 'ethers';
 
 const LoginCadastro = () => {
   const [error, setError] = useState('');
+  const [wallet, setWallet] = useState('');
+  const [sign, setSign] = useState('login');
+  const [name, setName] = useState('');
+
+  const handleChange = event => {
+    setName(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
 
   const erroDiv = <div>{error}</div>
 
-  return (
+  const login = (
     <section className=" bg-gradient-to-r from-green-100 to-white   w-screen h-screen flex flex-col justify-center items-center" >
       <div className="bg-gradient-to-r from-green-300 via-green-500 to-green-700 w-[50vh] h-[50vh] max-w-[90vw] gap-y-8 flex flex-col justify-center items-center rounded-lg" >
 
         <h3 className="text-[#333] font-semibold text-3xl" >Login</h3>
 
-        <div className='w-[90%] flex flex-col justify-center items-center gap-y-2 ' >
-          <button onClick={() => { doSignIn() }} className="bg-black h-8 text-white rounded-md w-[50%] text-xl" >Entrar</button>
-          <button onClick={() => { doSignUp() }} className="bg-black h-8 text-white rounded-md w-[50%] text-xl" >Cadastrar</button>
-        </div>
+
+        <button onClick={() => { doSignIn() }} className="bg-black h-8 text-white rounded-md w-[50%] text-xl" >Entrar</button>
+
+        <hr className="w-[40%] border border-black" />
+        <button onClick={() => { setSign('cadastro') }} >
+          <p className="text-[#eee]" >Cadastrar</p>
+        </button>
+
+        {/* <button onClick={() => { doSignUp() }} className="bg-black h-8 text-white rounded-md w-[50%] text-xl" >Cadastrar</button> */}
+
 
         {error && erroDiv}
 
@@ -22,12 +38,42 @@ const LoginCadastro = () => {
     </section>
   );
 
+  const cadastro = (
+    <section className=" bg-gradient-to-r from-green-100 to-white   w-screen h-screen flex flex-col justify-center items-center" >
+      <div className="bg-gradient-to-r from-green-300 via-green-500 to-green-700 w-[50vh] h-[50vh] max-w-[90vw] gap-y-8 flex flex-col justify-center items-center rounded-lg" >
+
+        <h3 className="text-[#333] font-semibold text-3xl" >Cadastrar</h3>
+
+        <input value={name} onChange={handleChange} type="text" placeholder="Name" className="rounded-sm text-2xl w-[50%] rounded-lg" />
+
+        <button onClick={() => { doSignUp() }} className="bg-black h-8 text-white rounded-md w-[50%] text-xl" >Cadastrar</button>
+
+        <hr className="w-[40%] border border-black" />
+        <button onClick={() => { setSign('login') }} >
+          <p className="text-[#eee]" >Login</p>
+        </button>
+
+        {/* <button onClick={() => { doSignUp() }} className="bg-black h-8 text-white rounded-md w-[50%] text-xl" >Cadastrar</button> */}
+
+
+        {error && erroDiv}
+
+      </div>
+    </section>
+  );
+
+  return (
+    sign === "login" ? login : cadastro
+  );
+
+
+
 
   function doSignIn() {
-    connect()
+    connect('login')
       .then(credentials => {
         console.log(credentials);
-        fetch('http://localhost:8081/login', {
+        fetch('http://localhost:8080/login', {
           method: 'POST',
           credentials: 'include',
           body: JSON.stringify(credentials),
@@ -46,9 +92,9 @@ const LoginCadastro = () => {
 
 
   function doSignUp() {
-    connect()
+    connect('cadastro')
       .then(credentials => {
-        fetch('http://localhost:8081/new-user', {
+        fetch('http://localhost:8080/new-user', {
           method: 'POST',
           credentials: 'include',
           body: JSON.stringify(credentials),
@@ -65,9 +111,10 @@ const LoginCadastro = () => {
       });
   }
 
-  async function connect() {
+  async function connect(value) {
+    let response;
     try {
-      const response = await fetch('http://localhost:8081/get-message', {
+      response = await fetch('http://localhost:8080/get-message', {
         credentials: 'include',
       });
     } catch (err) {
@@ -93,7 +140,11 @@ const LoginCadastro = () => {
     const signer = provider.getSigner();
     const password = await signer.signMessage(SECRET);
 
-    return { personalWallet: accounts[0], password, userName: 'hugo' };
+    if (value === 'login') {
+      return { personalWallet: accounts[0], password }
+    }
+
+    return { personalWallet: accounts[0], password, userName: name }
   }
 
 }
