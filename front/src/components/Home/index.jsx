d99e2c5e3124281178a219d23bad1e56a7dae638
Felipe Bueno <felipebuenonunes16@gmail.com>
2022-09-28 02:35:05 -0300

    feat: new style pages

--- front/src/components/Home/index.jsx
+++ front/src/components/Home/index.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Header } from '../Header';
 import { useUser } from '../../providers/user';
 import { get } from '../../functions/req'
 import { post } from '../../functions/req';
@@ -10,8 +9,12 @@ const Home = () => {
   const [balance, setBalance] = useState('');
   const [name, setName] = useState('');
   const { setBueno } = useUser();
-  const [activeMenu, setActiveMenu] = useState(false);
-  const [buyChips, setBuyChips] = useState(false);
+
+  const exitButton = () => {
+    document.cookie = "pachin-game/cookie" + "=" +
+      ";expires=Thu, 01 Jan 1970 00:00:01 GMT";
+    navigation("/");
+  }
 
   const getBalance = async () => {
     return get("/wallet/balance")
@@ -34,86 +37,114 @@ const Home = () => {
   }, []);
 
   const getToken = () => {
-    post('/wallet/buy_token', { "amount": "100" })
+    post('/wallet/buy_token', { "amount": "50" })
       .then(res => res.success && getBalance())
       .catch(err => console.err(err));
   };
 
+  const buttonPlay = () => {
+    if(balance <= 0) return;
+    navigation('/table');
+  }
+
   return (
-    <section className='bg-BJgreen01 w-full h-screen' >
-      <Header className={`h-[5%] md:h-[10%]`} />
-
-      {/* button opem modal info player */}
-      <button
-        className='text-5xl leading-[3px] text-white absolute top-0 right-2 md:leading-10'
-        onClick={() => {
-          activeMenu === false ? setActiveMenu(true) : setActiveMenu(false)
-        }} >
-        ...
-      </button>
-
-      {/* modal info player */}
-      <div className={` InfoModal ${activeMenu === true ? 'flex' : 'hidden'} absolute top-0 left-0  flex flex-col justify-center items-center z-10  w-screen h-screen backdrop-blur-sm bg-black/90`} >
-        <div className=' Container w-[50vh] h-[50vh] bg-BJgreen01/60 relative flex flex-col justify-center items-center gap-y-8 ' >
-          <button
-            className=' ButtonClose absolute top-0 right-0 bg-red-500 w-[10%] h-[10%]'
-            onClick={() => {
-              activeMenu === false ? setActiveMenu(true) : setActiveMenu(false)
-            }}
-          >
-            X
+    <section className='w-full h-[100vh] flex'>
+      <div className='h-[100vh] bg-[#910023] w-[48%] flex justify-around flex-col items-center'>
+        <div className='w-full flex justify-between px-20' >
+          <button className='flex items-center text-white font-bold text-3xl' onClick={exitButton}>
+            <img className="mr-3" src="/assets/arrow.svg" alt="arrow" />
+            Sair
           </button>
-
-          <div className=' ContentText bg-BJbrown text-white text-3xl w-[80%] px-4 py-2 gap-y-4 rounded-lg ' >
-            <p>balance: {balance}</p>
-            <p>name: {name}</p>
-          </div>
-
-          <button onClick={getToken} className='text-yellow-300 text-2xl bg-BJbrown/60 px-4 py-2 rounded-lg ' >comprar moedas</button>
+          <button className='text-white text-3xl' onClick={getToken}>Comprar moedas</button>
         </div>
+        <h1 className='text-white font-extrabold text-8xl' >{name}</h1>
+        <div className='w-[16.5rem] h-[9.5rem] border-2 border-white rounded-[2.2rem] flex justify-center items-center' >
+          <span className='text-white font-bold text-3xl'>Saldo:</span>
+          <p className='text-white text-3xl ml-2'>{balance}</p>
+        </div>
+        <button className='bg-white w-[21rem] h-[4rem] text-[#1A1A32] text-3xl font-bold rounded-[1.2rem]' onClick={() => buttonPlay()}>JOGAR</button>
       </div>
-
-      {/* modal buy chips */}
-      <div className={` InfoModal ${buyChips === true ? 'flex' : 'hidden'} absolute top-0 left-0  flex flex-col justify-center items-center z-10  w-screen h-screen backdrop-blur-sm bg-black/90`} >
-        <div className=' Container w-[50vh] h-[50vh] bg-BJgreen01/60 relative flex flex-col justify-center items-center gap-y-8 ' >
-          <button
-            className=' ButtonClose absolute top-0 right-0 bg-red-500 w-[10%] h-[10%]'
-            onClick={() => {
-              buyChips === false ? setBuyChips(true) : setBuyChips(false)
-            }}
-          >
-            X
-          </button>
-
-          <div className=' ContentText bg-BJbrown text-white text-3xl w-[80%] px-4 py-2 gap-y-4 rounded-lg ' >
-            <p className='text-4xl text-yellow-300' >Compre fichas</p>
-            <p>balance: {balance}</p>
-            <p>name: {name}</p>
-          </div>
-
-          <button onClick={getToken} className='text-yellow-300 text-2xl bg-BJbrown/60 px-4 py-2 rounded-lg ' >comprar moedas</button>
-        </div>
+      <div className='h-[100vh] w-[52%] flex items-center justify-center'>
+        <img src="/assets/big-logo.svg" alt="logo" />
       </div>
-
-      <main className=' w-full h-[95%] md:h-[90%] flex flex-col justify-center items-center' >
-        <div
-          style={{ backgroundImage: `url(/assets/image-dragon.png)`, backgroundRepeat: "no-repeat", backgroundSize: 'cover' }}
-          className={` w-[50vh] h-[50vh] max-w-[90%] flex flex-col justify-center items-center gap-y-8`} >
-
-
-
-          <div className=' ContainerContent backdrop-blur-[2px] bg-black/20 w-[50vh] h-[50vh] max-w-[90%] flex flex-col justify-center items-center gap-y-8' >
-            <h1 className=' text-BJwhite font-bold text-4xl' >Dragon Jack</h1>
-            <button
-              className=' PlayButton bg-NTgreenDark text-BJwhite font-bold text-3xl rounded-lg px-4 py-2' onClick={() => { balance > 0 ? navigation('/table') : console.log('compre fichas primeiro'), setBuyChips(true) }}
-            >
-              JOGAR
-            </button>
-          </div>
-
-        </div>
-      </main>
-    </section >
+    </section>
   )
+
+  // return (
+  //   <section className='bg-BJgreen01 w-full h-screen' >
+  //     <Header className={`h-[5%] md:h-[10%]`} />
+
+  //     {/* button opem modal info player */}
+  //     <button
+  //       className='text-5xl leading-[3px] text-white absolute top-0 right-2 md:leading-10'
+  //       onClick={() => {
+  //         activeMenu === false ? setActiveMenu(true) : setActiveMenu(false)
+  //       }} >
+  //       ...
+  //     </button>
+
+  //     {/* modal info player */}
+  //     <div className={` InfoModal ${activeMenu === true ? 'flex' : 'hidden'} absolute top-0 left-0  flex flex-col justify-center items-center z-10  w-screen h-screen backdrop-blur-sm bg-black/90`} >
+  //       <div className=' Container w-[50vh] h-[50vh] bg-BJgreen01/60 relative flex flex-col justify-center items-center gap-y-8 ' >
+  //         <button
+  //           className=' ButtonClose absolute top-0 right-0 bg-red-500 w-[10%] h-[10%]'
+  //           onClick={() => {
+  //             activeMenu === false ? setActiveMenu(true) : setActiveMenu(false)
+  //           }}
+  //         >
+  //           X
+  //         </button>
+
+  //         <div className=' ContentText bg-BJbrown text-white text-3xl w-[80%] px-4 py-2 gap-y-4 rounded-lg ' >
+  //           <p>balance: {balance}</p>
+  //           <p>name: {name}</p>
+  //         </div>
+
+  //         <button onClick={getToken} className='text-yellow-300 text-2xl bg-BJbrown/60 px-4 py-2 rounded-lg ' >comprar moedas</button>
+  //       </div>
+  //     </div>
+
+  //     {/* modal buy chips */}
+  //     <div className={` InfoModal ${buyChips === true ? 'flex' : 'hidden'} absolute top-0 left-0  flex flex-col justify-center items-center z-10  w-screen h-screen backdrop-blur-sm bg-black/90`} >
+  //       <div className=' Container w-[50vh] h-[50vh] bg-BJgreen01/60 relative flex flex-col justify-center items-center gap-y-8 ' >
+  //         <button
+  //           className=' ButtonClose absolute top-0 right-0 bg-red-500 w-[10%] h-[10%]'
+  //           onClick={() => {
+  //             buyChips === false ? setBuyChips(true) : setBuyChips(false)
+  //           }}
+  //         >
+  //           X
+  //         </button>
+
+  //         <div className=' ContentText bg-BJbrown text-white text-3xl w-[80%] px-4 py-2 gap-y-4 rounded-lg ' >
+  //           <p className='text-4xl text-yellow-300' >Compre fichas</p>
+  //           <p>balance: {balance}</p>
+  //           <p>name: {name}</p>
+  //         </div>
+
+  //         <button onClick={getToken} className='text-yellow-300 text-2xl bg-BJbrown/60 px-4 py-2 rounded-lg ' >comprar moedas</button>
+  //       </div>
+  //     </div>
+
+  //     <main className=' w-full h-[95%] md:h-[90%] flex flex-col justify-center items-center' >
+  //       <div
+  //         style={{ backgroundImage: `url(/assets/image-dragon.png)`, backgroundRepeat: "no-repeat", backgroundSize: 'cover' }}
+  //         className={` w-[50vh] h-[50vh] max-w-[90%] flex flex-col justify-center items-center gap-y-8`} >
+
+
+
+  //         <div className=' ContainerContent backdrop-blur-[2px] bg-black/20 w-[50vh] h-[50vh] max-w-[90%] flex flex-col justify-center items-center gap-y-8' >
+  //           <h1 className=' text-BJwhite font-bold text-4xl' >Dragon Jack</h1>
+  //           <button
+  //             className=' PlayButton bg-NTgreenDark text-BJwhite font-bold text-3xl rounded-lg px-4 py-2' onClick={() => { balance > 0 ? navigation('/table') : console.log('compre fichas primeiro'), setBuyChips(true) }}
+  //           >
+  //             JOGAR
+  //           </button>
+  //         </div>
+
+  //       </div>
+  //     </main>
+  //   </section >
+  // )
 }
 export { Home };
--- front/src/components/LoginCadastro/index.jsx
+++ front/src/components/LoginCadastro/index.jsx
@@ -25,67 +25,97 @@ const LoginCadastro = () => {
 
   const navigation = useNavigate();
 
-  const handleChange = event => {
-    setName(event.target.value);
-
-    console.log('value is:', event.target.value);
-  };
+  const handleChange = event => setName(event.target.value);
 
   const erroDiv = <div>{error}</div>
 
-  const login = (
-    <section className=" bg-BJgreen01 w-screen h-screen flex flex-col justify-center items-center" >
+  // const login = (
+  //   <section className=" bg-BJgreen01 w-screen h-screen flex flex-col justify-center items-center" >
 
-      <div
-        style={{ backgroundImage: `url(/assets/image-dragon.png)`, backgroundRepeat: "no-repeat", backgroundSize: 'cover' }}
-        className=" text-white w-[50vh] h-[50vh] max-w-[90vw] gap-y-8 flex flex-col justify-center items-center rounded-lg" >
+  //     <div
+  //       style={{ backgroundImage: `url(/assets/image-dragon.png)`, backgroundRepeat: "no-repeat", backgroundSize: 'cover' }}
+  //       className=" text-white w-[50vh] h-[50vh] max-w-[90vw] gap-y-8 flex flex-col justify-center items-center rounded-lg" >
 
-        <div className='backdrop-blur-[2px] bg-black/80 w-full h-full flex flex-col justify-center items-center gap-y-8' >
+  //       <div className='backdrop-blur-[2px] bg-black/80 w-full h-full flex flex-col justify-center items-center gap-y-8' >
 
-          <h3 className="text-white font-semibold text-4xl" >Login</h3>
-          <button onClick={() => { doSignIn() }} className="bg-NTgreenDark h-8 text-white rounded-md w-[50%] text-xl" >Entrar</button>
+  //         <h3 className="text-white font-semibold text-4xl" >Login</h3>
+  //         <button onClick={() => { doSignIn() }} className="bg-NTgreenDark h-8 text-white rounded-md w-[50%] text-xl" >Entrar</button>
 
-          <hr className="w-[40%] border border-green-400" />
+  //         <hr className="w-[40%] border border-green-400" />
 
-          <button onClick={() => { setSign('cadastro') }} >
-            <p className="text-[#eee]" >Cadastrar</p>
-          </button>
+  //         <button onClick={() => { setSign('cadastro') }} >
+  //           <p className="text-[#eee]" >Cadastrar</p>
+  //         </button>
+  //       </div>
+
+  //       {error && erroDiv}
+
+  //     </div>
+  //   </section>
+  // );
+
+  const Left = () => {
+    let [isCadaster, setIsCadaster] = useState(false)
+    return (
+      <div className='w-[45%] h-[100vh] bg-white flex justify-center items-center'>
+        <div className='border-2 w-[32rem] h-[24rem] border-[#1A1A32] rounded-3xl flex flex-col justify-center items-center'>
+          {
+            isCadaster ?
+              <input className='border-2 border-[#212121] w-[21rem] h-[4rem] text-[#212120] text-2xl font-bold rounded-[1.2rem] pl-4' type="text" placeholder='username' id="name-input" /> :
+              <button className='bg-[#1A1A32] w-[21rem] h-[4rem] text-white text-3xl font-bold rounded-[1.2rem]' onClick={() => { doSignIn() }}>Logar</button>
+          }
+          <button className='mb-3 bg-[#1A1A32] w-[21rem] h-[4rem] text-white text-3xl font-bold rounded-[1.2rem] mt-9' onClick={() => {
+            if (!isCadaster) return setIsCadaster(true);
+            doSignUp();
+          }}>Registre-se</button>
+          <button className={`${!isCadaster && 'hidden'}`} onClick={() => setIsCadaster(false)}>Já possue uma conta? clique aqui</button>
         </div>
-
-        {error && erroDiv}
-
+        <img className="absolute bottom-3" src="/assets/logo.svg" alt="logo" />
       </div>
-    </section>
-  );
+    )
+  }
 
-  const cadastro = (
-    <section className=" bg-BJgreen01  w-screen h-screen flex flex-col justify-center items-center" >
-      <div
-        style={{ backgroundImage: `url(/assets/image-dragon.png)`, backgroundRepeat: "no-repeat", backgroundSize: 'cover' }}
-        className="w-[50vh] h-[50vh] max-w-[90vw] gap-y-8 flex flex-col justify-center items-center rounded-lg"
-      >
-
-        <div className='backdrop-blur-[2px] bg-black/80 flex flex-col justify-center items-center gap-y-8 w-full h-full' >
-
-          <h3 className="text-white font-semibold text-3xl" >Cadastrar</h3>
-          <input value={name} onChange={handleChange} type="text" placeholder="Name" className="rounded-sm text-2xl w-[50%]" />
-          <button onClick={() => { doSignUp() }} className="bg-NTgreenDark h-8 text-white rounded-md w-[50%] text-xl" >Cadastrar</button>
-
-          <hr className="w-[40%] border border-green-400" />
-
-          <button onClick={() => { setSign('login') }} >
-            <p className="text-[#eee]" >Login</p>
-          </button>
-
-          {error && erroDiv}
+  const Rigth = () => {
+    return (
+      <div className='w-[55%] h-[100vh] bg-[#910023] flex justify-center items-center'>
+        <div>
+          <img src="/assets/dragon.png" alt="dragon" />
         </div>
-
       </div>
-    </section>
-  );
+    )
+  }
+  // const cadastro = (
+  //   <section className=" bg-BJgreen01  w-screen h-screen flex flex-col justify-center items-center" >
+  //     <div
+  //       style={{ backgroundImage: `url(/assets/image-dragon.png)`, backgroundRepeat: "no-repeat", backgroundSize: 'cover' }}
+  //       className="w-[50vh] h-[50vh] max-w-[90vw] gap-y-8 flex flex-col justify-center items-center rounded-lg"
+  //     >
+
+  //       <div className='backdrop-blur-[2px] bg-black/80 flex flex-col justify-center items-center gap-y-8 w-full h-full' >
+
+  //         <h3 className="text-white font-semibold text-3xl" >Cadastrar</h3>
+  //         <input value={name} onChange={handleChange} type="text" placeholder="Name" className="rounded-sm text-2xl w-[50%]" />
+  //         <button onClick={() => { doSignUp() }} className="bg-NTgreenDark h-8 text-white rounded-md w-[50%] text-xl" >Cadastrar</button>
+
+  //         <hr className="w-[40%] border border-green-400" />
+
+  //         <button onClick={() => { setSign('login') }} >
+  //           <p className="text-[#eee]" >Login</p>
+  //         </button>
+
+  //         {error && erroDiv}
+  //       </div>
+
+  //     </div>
+  //   </section>
+  // );
 
   return (
-    sign === "login" ? login : cadastro
+    //sign === "login" ? login : cadastro
+    <div className='flex'>
+      <Left />
+      <Rigth />
+    </div>
   );
 
   function doSignIn() {
@@ -116,7 +146,7 @@ const LoginCadastro = () => {
   function doSignUp() {
     connect('cadastro')
       .then(credentials => {
-        fetch(process.env.REACT_APP_URL+'/new-user', {
+        fetch(process.env.REACT_APP_URL + '/new-user', {
           method: 'POST',
           credentials: 'include',
           body: JSON.stringify(credentials),
@@ -138,16 +168,15 @@ const LoginCadastro = () => {
   async function connect(value) {
     let response;
     try {
-      response = await fetch(process.env.REACT_APP_URL+'/get-message', {
+      response = await fetch(process.env.REACT_APP_URL + '/get-message', {
         credentials: 'include',
       });
     } catch (err) {
-      setError(err.message);
+      console.error(err);
     }
 
     const message = await response.json();
     const SECRET = message.data;
-    // TODO adicionar campo de nome de usuário no front . Estamos com o userName da resposta hardcoded
 
     setError('');
 
@@ -167,7 +196,7 @@ const LoginCadastro = () => {
     if (value === 'login') {
       return { personalWallet: accounts[0], password }
     }
-
+    const name = document.getElementById('name-input').value
     return { personalWallet: accounts[0], password, userName: name }
   }
 
